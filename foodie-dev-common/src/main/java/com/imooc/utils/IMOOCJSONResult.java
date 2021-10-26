d4e7f930630f06fee217daa61e5822297f8c58e9
zhangjinyu <zhangjnyu@qiaojizixun.com>
2021-10-26 18:39:19 +0800

    11.26下午

--- foodie-dev-common/src/main/java/com/imooc/utils/IMOOCJSONResult.java
+++ foodie-dev-common/src/main/java/com/imooc/utils/IMOOCJSONResult.java
@@ -14,6 +14,38 @@ public class IMOOCJSONResult {
     // 响应消息
     private String msg;
 
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public String getOk() {
+        return ok;
+    }
+
+    public void setOk(String ok) {
+        this.ok = ok;
+    }
+
     // 响应中的数据
     private Object data;
 
