0c46df71f1cb28e6edd702f429b47a77dfdd26e9
zhangjinyu <363626256@qq.com>
2021-10-27 23:02:17 +0800

    27晚

---
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.imooc.service.impl;
+
+import com.imooc.mapper.UsersMapper;
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+import com.imooc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tk.mybatis.mapper.entity.Example;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    public UsersMapper usersMapper;
+
+    @Autowired
+    //public Sid sid;
+
+    @Override
+    public boolean queryUsernameIsExist(String username) {
+        Example userExample = new Example(Users.class);
+        Example.Criteria criteria = userExample.createCriteria();
+        criteria.andEqualTo("username", username);
+
+
+        return false;
+    }
+
+    @Override
+    public Users createUser(UserBO userBO) {
+        return null;
+    }
+
+    @Override
+    public Users queryUserForLogin(String username, String password) {
+        return null;
+    }
+}
