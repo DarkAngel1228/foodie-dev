25774ed3b5183a262f95c2f6e8de565b375791e2
zhangjinyu <363626256@qq.com>
2021-10-30 08:48:40 +0800

    商品详情&商品评论的数量

---
+++ foodie-dev-api/src/main/java/com/imooc/controller/ItemsController.java
@@ -0,0 +1,63 @@
+package com.imooc.controller;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemInfoVO;
+import com.imooc.service.ItemService;
+import com.imooc.utils.IMOOCJSONResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Api(value = "商品接口", tags = {"商品信息展示的相关接口"})
+@RestController
+@RequestMapping("item")
+public class ItemsController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @ApiOperation(value = "查询商品详情", notes = "查询商品详情", httpMethod = "GET")
+    @GetMapping("/info/{itemId}")
+    public IMOOCJSONResult info(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @PathVariable String itemId) {
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+        Items items = itemService.queryItemById(itemId);
+        List<ItemsImg> itemsImgs = itemService.queryItemImgList(itemId);
+        List<ItemsSpec> itemsSpecs = itemService.queryItemSpecList(itemId);
+        ItemsParam itemsParam = itemService.queryItemParam(itemId);
+
+        ItemInfoVO itemInfoVO = new ItemInfoVO();
+        itemInfoVO.setItem(items);
+        itemInfoVO.setItemImgList(itemsImgs);
+        itemInfoVO.setItemParams(itemsParam);
+        itemInfoVO.setItemSpecList(itemsSpecs);
+
+        return IMOOCJSONResult.ok(itemInfoVO);
+    }
+
+    @ApiOperation(value = "查询商品评价等级", notes = "查询商品评价等级", httpMethod = "GET")
+    @GetMapping("/commentLevel")
+    public IMOOCJSONResult commentLevel(
+            @ApiParam(name = "itemId", value = "商品id", required = true)
+            @RequestParam String itemId) {
+        if (StringUtils.isBlank(itemId)) {
+            return IMOOCJSONResult.errorMsg(null);
+        }
+
+        CommentLevelCountsVO commentLevelCountsVO = itemService.queryCommentCounts(itemId);
+
+        return IMOOCJSONResult.ok(commentLevelCountsVO);
+    }
+}
---
+++ foodie-dev-service/src/main/java/com/imooc/service/impl/ItemServiceImpl.java
@@ -0,0 +1,102 @@
+package com.imooc.service.impl;
+
+import com.imooc.enums.CommentLevel;
+import com.imooc.mapper.*;
+import com.imooc.pojo.*;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemInfoVO;
+import com.imooc.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private ItemsMapper itemsMapper;
+
+    @Autowired
+    private ItemsImgMapper itemsImgMapper;
+
+    @Autowired
+    private ItemsSpecMapper itemsSpecMapper;
+
+    @Autowired
+    private ItemsParamMapper itemsParamMapper;
+
+    @Autowired
+    private ItemsCommentsMapper itemsCommentsMapper;
+
+    @Autowired
+    private ItemsMapperCustom itemsMapperCustom;
+
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Items queryItemById(String itemId) {
+        return itemsMapper.selectByPrimaryKey(itemId);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsImg> queryItemImgList(String itemId) {
+        Example itemsImgExp = new Example(ItemsImg.class);
+        Example.Criteria itemsImgExpCriteria = itemsImgExp.createCriteria();
+        itemsImgExpCriteria.andEqualTo("itemId", itemId);
+
+        return itemsImgMapper.selectByExample(itemsImgExp);
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public List<ItemsSpec> queryItemSpecList(String itemId) {
+        Example example = new Example(ItemsSpec.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsSpecMapper.selectByExample(example);
+    }
+
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public ItemsParam queryItemParam(String itemId) {
+        Example example = new Example(ItemsParam.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("itemId", itemId);
+
+        return itemsParamMapper.selectOneByExample(example);
+    }
+
+    @Override
+    public CommentLevelCountsVO queryCommentCounts(String itemId) {
+        Integer goodCounts = getCommentCounts(itemId, CommentLevel.GOOD.type);
+        Integer normalCounts = getCommentCounts(itemId, CommentLevel.NORMAL.type);
+        Integer badCounts = getCommentCounts(itemId, CommentLevel.BAD.type);
+        Integer totalCounts = goodCounts + normalCounts + badCounts;
+
+        CommentLevelCountsVO commentLevelCountsVO = new CommentLevelCountsVO();
+        commentLevelCountsVO.setGoodCounts(goodCounts);
+        commentLevelCountsVO.setNormalCounts(normalCounts);
+        commentLevelCountsVO.setBadCounts(badCounts);
+        commentLevelCountsVO.setTotalCounts(totalCounts);
+
+        return commentLevelCountsVO;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    Integer getCommentCounts(String itemId, Integer level) {
+        ItemsComments itemsComments = new ItemsComments();
+        itemsComments.setItemId(itemId);
+        if (level != null) {
+            itemsComments.setCommentLevel(level);
+        }
+
+        return itemsCommentsMapper.selectCount(itemsComments);
+    }
+}
