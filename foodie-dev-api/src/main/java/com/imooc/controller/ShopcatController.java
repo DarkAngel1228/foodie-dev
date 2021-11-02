9a0ae38a429025442d5dcaf3ece4e23057bc4046
zhangjinyu <363626256@qq.com>
2021-11-03 07:21:39 +0800

    redis

--- foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
@@ -2,18 +2,26 @@ package com.imooc.controller;
 
 import com.imooc.pojo.bo.ShopcartBO;
 import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.JsonUtils;
+import com.imooc.utils.RedisOperator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(value = "购物车相关接口", tags = {"购物车相关接口"})
 @RequestMapping("shopcart")
 @RestController
-public class ShopcatController {
+public class ShopcatController extends BaseController{
+
+    @Autowired
+    private RedisOperator redisOperator;
 
     @ApiOperation(value = "添加商品到购物车", notes = "添加商品到购物车", httpMethod = "POST")
     @PostMapping("/add")
@@ -26,7 +34,31 @@ public class ShopcatController {
             return IMOOCJSONResult.errorMsg("");
         }
         System.out.println(shopcartBO);
-        // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
+        // 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        List<ShopcartBO> shopcartList = null;
+
+        if (StringUtils.isNotBlank(shopcartJson)) {
+            shopcartList = JsonUtils.jsonToList(shopcartJson, ShopcartBO.class);
+
+            boolean isHaving = false;
+            for (ShopcartBO sc : shopcartList) {
+                String specId = sc.getSpecId();
+                if (specId.equals(shopcartBO.getSpecId())) {
+                    sc.setBuyCounts(sc.getBuyCounts() + shopcartBO.getBuyCounts());
+                    isHaving = true;
+                }
+            }
+
+            if (! isHaving) {
+                shopcartList.add(shopcartBO);
+            }
+        } else {
+            shopcartList = new ArrayList<>();
+            shopcartList.add(shopcartBO);
+        }
+
+        redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopcartList));
 
         return IMOOCJSONResult.ok();
     }
@@ -42,7 +74,9 @@ public class ShopcatController {
             return IMOOCJSONResult.errorMsg("");
         }
 
-        // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+        // 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+
         return IMOOCJSONResult.ok();
     }
 }
